1d18859cbfa8b2cbd7e8b3d7d53841dcd65f5d45
Stamatios Galiatsatos <19597222+Logicenios@users.noreply.github.com>
2023-05-01 17:40:57 +0300

    Flask Init

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from picamera import PiCamera
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
+
+#Running the webpage
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+# Starting the camera live preview and the face detection
+
+piwatch = Picamera2()
+video_config = piwatch.create_video_configuration()
+piwatch.configure(video_config)
+
+encoder = H264Encoder(10000000)
+output = FfmpegOutput('preview.mp4')
+
+piwatch.start_recording(encoder, output)
+
