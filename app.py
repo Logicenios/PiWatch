d760bf19f1460115807c52f290a3234e133bd4a2
Stamatios Galiatsatos <19597222+Logicenios@users.noreply.github.com>
2023-05-01 22:25:19 +0300

    Change to camera streamer

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from video import start_video , stop_video, take_picture
 #Running the webpage
 
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app = Flask(__name__)
 def index():
     
     return render_template('index.html')
-    start_video()
+
 
 if __name__ == '__main__':
     app.run()
