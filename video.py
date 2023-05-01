9cad20ca8bebf73b99b264d2f63a9c83824344e5
Stamatios Galiatsatos <19597222+Logicenios@users.noreply.github.com>
2023-05-01 20:13:42 +0300

    Update video.py

--- video.py
+++ video.py
@@ -2,15 +2,16 @@ from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration(main={"size": (1920, 1080),"frame_rate": 30})
+video_config = picam2.create_video_configuration(main={"size": (1920, 1080)})
 picam2.configure(video_config)
 encoder = JpegEncoder(q=70)
 #picam2.start_preview()
 
 def take_picture():
-    picam2.capture('motion.jpg')
+    picam2.capture('/static/images/motion.jpg')
 def start_video():
-    picam2.start_recording(encoder, 'test.mjpg')
+    picam2.start_recording(encoder, '/static/images/test.mjpg')
 #picam2.start_recording(encoder, 'test.mjpg')
 def stop_video():
     picam2.stop_recording()
+
