46321e6d6f5db79a62eb3884074e29880222a62f
Stamatios Galiatsatos <19597222+Logicenios@users.noreply.github.com>
2023-05-01 14:11:53 +0300

    Trying webserver

--- opencvtest.py
+++ opencvtest.py
@@ -1,46 +1,96 @@
 #!/usr/bin/python3
-import time
 
-import cv2
+# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
+# Run this script, then point a web browser at http:<this-ip-address>:8000
+# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
 
-from picamera2 import MappedArray, Picamera2, Preview
-from picamera2.encoders import H264Encoder
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition
 
-# This is like opencv_face_detect_2.py, only we draw the face boxes on a
-# recorded video. We have to use the "post_callback" to draw the faces because
-# we want capture_buffer to get the image without face boxes, but then we
-# want the boxes drawn before handing the image to the encoder.
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 
-face_detector = cv2.CascadeClassifier("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml")
+PAGE = """\
+<html>
+<head>
+<title>picamera2 MJPEG streaming demo</title>
+</head>
+<body>
+<h1>Picamera2 MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="640" height="480" />
+</body>
+</html>
+"""
 
 
-def draw_faces(request):
-    with MappedArray(request, "main") as m:
-        for f in faces:
-            (x, y, w, h) = [c * n // d for c, n, d in zip(f, (w0, h0) * 2, (w1, h1) * 2)]
-            cv2.rectangle(m.array, (x, y), (x + w, y + h), (0, 255, 0, 0))
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
 
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
-config = picam2.create_preview_configuration(main={"size": (640, 480)},
-                                             lores={"size": (320, 240), "format": "YUV420"})
-picam2.configure(config)
+picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+output = StreamingOutput()
+picam2.start_recording(JpegEncoder(), FileOutput(output))
 
-(w0, h0) = picam2.stream_configuration("main")["size"]
-(w1, h1) = picam2.stream_configuration("lores")["size"]
-s1 = picam2.stream_configuration("lores")["stride"]
-faces = []
-picam2.post_callback = draw_faces
-
-encoder = H264Encoder(10000000)
-picam2.start_recording(encoder, "test.h264")
-
-start_time = time.monotonic()
-# Run for 10 seconds.
-while time.monotonic() - start_time < 10:
-    buffer = picam2.capture_buffer("lores")
-    grey = buffer[:s1 * h1].reshape((h1, s1))
-    faces = face_detector.detectMultiScale(grey, 1.1, 3)
-
-picam2.stop_recording()
+try:
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
