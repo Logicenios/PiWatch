edde56d107640ef3c0a988f77e16aa88df699f5c
Stamatios Galiatsatos <19597222+Logicenios@users.noreply.github.com>
2023-05-01 23:00:06 +0300

    Removed Nginx

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 from flask import Flask, render_template
-from werkzeug.middleware.proxy_fix import ProxyFix
-
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
-
-
 
 #Running the webpage
 
