6c1d4617f3eee3a05850b6e7e3c88904b19c0ceb
Stamatios Galiatsatos <19597222+Logicenios@users.noreply.github.com>
2023-05-01 20:07:26 +0300

    Maybe a working demo?

--- app.py
+++ app.py
@@ -1,37 +1,16 @@
 from flask import Flask, render_template
-from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
-from picamera2.outputs import FfmpegOutput
-import libcamera
-import time
-
+from video import start_video , stop_video, take_picture
 #Running the webpage
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    start_video()
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
-
-# Starting the camera live preview and the face detection
-
-piwatch = Picamera2()
-piwatch.rotation = 180
-piwatch.frame_rate = 25
-piwatch.resolution = (640, 480)
-video_config = piwatch.create_video_configuration()
-piwatch.configure(video_config)
-
-
-encoder = H264Encoder(10000000)
-output = FfmpegOutput('preview.mp4')
-
-piwatch.start_recording(encoder, output)
-time.sleep(5)
-piwatch.stop_recording()
 
 
 
--- video.py
+++ video.py
@@ -2,12 +2,15 @@ from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration(main={"size": (1920, 1080)})
+video_config = picam2.create_video_configuration(main={"size": (1920, 1080),"frame_rate": 30})
 picam2.configure(video_config)
-
-picam2.start_preview()
 encoder = JpegEncoder(q=70)
+#picam2.start_preview()
 
-picam2.start_recording(encoder, 'test.mjpeg')
-time.sleep(10)
-picam2.stop_recording()
+def take_picture():
+    picam2.capture('motion.jpg')
+def start_video():
+    picam2.start_recording(encoder, 'test.mjpg')
+#picam2.start_recording(encoder, 'test.mjpg')
+def stop_video():
+    picam2.stop_recording()
